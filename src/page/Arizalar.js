2aa2c2598834249e21d83e79efd83b3ecbad1491
DavronMamaraimov <davronmamaraimov1996@gmail.com>
2023-12-06 23:30:01 +0500

    continued

---
+++ src/page/Arizalar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const ArizaBox = styled.div`
+  height: 91vh;
+  /* background-color: red; */
+`;
+
+export default function Arizalar() {
+  return <ArizaBox>Mening Arizalarim</ArizaBox>;
+}
--- src/page/DashboardWrapper.js
+++ src/page/DashboardWrapper.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // import { Link } from "react-router-dom";
 import styled from "styled-components";
-// import Express24 from "../img/image 15 (1).png";
+import Express24 from "../img/image 15 (1).png";
+import Arizalar from "./Arizalar";
+import Header from "./Header";
 
 const DashboardBox = styled.section`
   /* border: 1px solid; */
@@ -15,8 +18,24 @@ const DashboardBox = styled.section`
     display: flex;
     flex-direction: column;
     width: 400px;
-    align-items: center;
-    padding: 20px 0px;
+    /* align-items: flex-start; */
+    gap: 30px;
+    padding: 20px;
+    .links {
+      /* border: 1px solid red; */
+      padding: 20px;
+      border-radius: 10px;
+      background-color: rgba(255, 220, 3, 0.65);
+      color: black;
+      font-size: 20px;
+      text-decoration: none;
+      font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+        "Lucida Sans", Arial, sans-serif;
+      transition: 0.5s;
+      &:hover {
+        background-color: rgba(155, 220, 3, 0.65);
+      }
+    }
     /* justify-content: center; */
     ul {
       padding: 0;
@@ -48,16 +67,16 @@ const DashboardBox = styled.section`
     flex-direction: column;
     header {
       height: 9vh;
-      background-color: red;
-      color: white;
+      /* background-color: red; */
+      color: black;
       /* border: 1px solid green; */
     }
 
     main {
       height: calc(100% - 8vh);
       /* border: 1px solid red; */
-      background-color: green;
-      color: white;
+      /* background-color: green; */
+      color: black;
     }
   }
 `;
@@ -66,34 +85,31 @@ export default function DashboardWrapper() {
   return (
     <DashboardBox>
       <aside className="m-2">
-        {/* <img src={Express24} alt="" />
-        <ul>
-          <li>
-            <Link className="link">
-              <span className="icon">o</span>
-              <span className="text">
-                <h4>Arizalar</h4>
-                <h5>Yetib kelgan arizalarni kuzatishingiz mumkin</h5>
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link>Yetkazilgan</Link>
-          </li>
-          <li>
-            <Link>Statistika</Link>
-          </li>
-          <li>
-            <Link>Qo`shish</Link>
-          </li>
-          <li>
-            <Link>Foydalanuvchilar</Link>
-          </li>
-        </ul> */}
+        <img src={Express24} alt="" />
+        <Link to={"/arizalar"} className="links">
+          Arizalar
+        </Link>
+        <Link to={"/yetkazilgan"} className="links">
+          Yetkazilgan
+        </Link>
+        <Link to={"/statistika"} className="links">
+          Statistika
+        </Link>
+        <Link to={"/qo'shish"} className="links">
+          Qo'shish
+        </Link>
+        <Link to={"/foydalanuvchilar"} className="links">
+          Foydalanuvchilar
+        </Link>
       </aside>
+
       <div className="rightside">
-        <header className="m-2 shadow">header</header>
-        <main className="m-2">main</main>
+        <header className="m-2 shadow">
+          <Header />
+        </header>
+        <main className="m-2">
+          <Arizalar />
+        </main>
       </div>
     </DashboardBox>
   );
