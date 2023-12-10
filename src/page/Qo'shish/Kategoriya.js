d13fe41e7d55046269f1b530a4fe067a7f8838b3
DavronMamaraimov <davronmamaraimov1996@gmail.com>
2023-12-10 14:32:53 +0500

    category in process

---
+++ src/page/Qo'shish/Kategoriya.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Kategoriya() {
+  return <div className="text-bg-danger">Kategoriya</div>;
+}
--- src/page/Qo'shish/Qoshish.js
+++ src/page/Qo'shish/Qoshish.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import DashboardTemplate from "../../components/DashboardTemplate";
+import styled from "styled-components";
+import Kategoriya from "./Kategoriya";
+
+const BtnGroup = styled.div`
+  /* border: 1px solid; */
+  display: flex;
+  width: 30%;
+  padding: 10px;
+  flex-direction: row;
+  gap: 10px;
+`;
 
 export default function Qoshish() {
-  return <DashboardTemplate>Qoshish</DashboardTemplate>;
+  const [state, setState] = useState(false);
+
+  return (
+    <DashboardTemplate>
+      <h1>Qo'shish</h1>
+      <p>Yangi kategoriya/taom qo'shish</p>
+
+      <BtnGroup>
+        <button
+          className="btn btn-warning fw-bold pe-4"
+          onClick={() => {
+            setState(!state);
+          }}
+        >
+          Kategoriya
+        </button>
+        <button className="btn btn-warning fw-bold pe-4">Taom</button>
+      </BtnGroup>
+      {state ? <Kategoriya /> : ""}
+    </DashboardTemplate>
+  );
 }
