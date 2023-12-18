8436955c6798a7a84e18b921d8cc994b67292f4c
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-12-18 16:40:45 +0500

    kategoriya done

--- src/page/Qo'shish/Qoshish.js
+++ src/page/Qo'shish/Qoshish.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import DashboardTemplate from "../../components/DashboardTemplate";
 import styled from "styled-components";
-import Kategoriya from "./Kategoriya";
+import { useSelector, useDispatch } from "react-redux";
 
 const BtnGroup = styled.div`
   /* border: 1px solid; */
@@ -28,8 +28,85 @@ const BtnGroup = styled.div`
   }
 `;
 
+const Category = styled.div`
+  /* border: 1px solid red; */
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  input {
+    border-radius: 10px;
+    width: 100%;
+    border: none;
+    outline: none;
+    border: 1px solid #c9c9c9;
+    padding: 12px 20px;
+    font-size: 20px;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+  }
+
+  button {
+    /* border: 1px solid red; */
+    border: none;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+    font-size: 20px;
+    font-weight: 600;
+    padding: 4px 40px;
+    box-shadow: 0px 2px 3px 3px #c9c9c9;
+    transition: 0.5s;
+    border-radius: 10px;
+    &:hover {
+      background-color: #ffec00;
+    }
+  }
+
+  ul {
+    width: 100%;
+    /* border: 1px solid #c9c9c9; */
+    /* padding: 0; */
+    /* padding: 12px 20px; */
+    font-size: 20px;
+    margin: 0;
+    padding: 0;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    li {
+      border-radius: 10px;
+      list-style-type: none;
+      border: 1px solid #c9c9c9;
+      padding: 12px 20px;
+      a {
+        text-decoration: none;
+        color: black;
+      }
+    }
+  }
+`;
+
 export default function Qoshish() {
-  const [state, setState] = useState(false);
+  const [value, setValue] = useState("");
+
+  const dispatch = useDispatch();
+  const category = useSelector((state) => state.category);
+  const addDisplay = useSelector((state) => state.addDisplay);
+
+  // console.log(category.title);
+  const setAddDisplay = () => {
+    dispatch({ type: "ADD_CATEGORY" });
+  };
+
+  const setCategory = () => {
+    if (value.trim() === "") return;
+    dispatch({ type: "SET_CATEGORY", payload: value });
+    setValue("");
+  };
 
   return (
     <DashboardTemplate>
@@ -37,17 +114,39 @@ export default function Qoshish() {
       <p>Yangi kategoriya/taom qo'shish</p>
 
       <BtnGroup>
-        <button
-          className="btn one"
-          onClick={() => {
-            setState(!state);
-          }}
-        >
+        <button className="btn one" onClick={setAddDisplay}>
           Kategoriya
         </button>
+
         <button className="btn second">Taom</button>
       </BtnGroup>
-      {state ? <Kategoriya /> : ""}
+
+      {/* {state ? <Kategoriya /> : ""} */}
+
+      {addDisplay ? (
+        <Category>
+          <input
+            type="text"
+            placeholder="Kategoriya nomi"
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+          />
+          <ul>
+            {category.map((item) => {
+              return (
+                <li>
+                  <a href="!#">{item.title}</a>
+                </li>
+              );
+            })}
+          </ul>
+          <button onClick={setCategory}>Qo`shish</button>
+        </Category>
+      ) : (
+        ""
+      )}
     </DashboardTemplate>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { createStore } from "redux";
 // import data from "../page/Foydalanuvchilar/data";
 
-const initialState = { edit: true, display: 1 };
+const initialState = {
+  category: [{ title: "Italian Pizza", completed: false }],
+  edit: true,
+  display: 1,
+  addDisplay: false,
+};
 
 const reducer = (state = initialState, action) => {
-  console.log(state.display);
+  console.log(state);
 
   switch (action.type) {
     case "EDIT":
@@ -17,6 +22,20 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         display: (state.display = 0),
+      };
+    case "ADD_CATEGORY":
+      return {
+        ...state,
+        addDisplay: !state.addDisplay,
+      };
+
+    case "SET_CATEGORY":
+      return {
+        ...state,
+        category: [
+          ...state.category,
+          { title: action.payload, completed: false },
+        ],
       };
     default:
       return state;
